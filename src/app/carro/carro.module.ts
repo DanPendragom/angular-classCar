a6164d45b402f7a3099f8a2dd36c57fdbaa87788
EtecAB <carlos.coutinho5@etec.sp.gov.br>
2019-10-23 15:19:41 -0300

    importando component

---
+++ src/app/carro/carro.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarroComponent } from './components';
+
+
+@NgModule({
+  declarations: [
+    CarroComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarroModule { }
